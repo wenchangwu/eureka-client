89fb9c54a37d6b9a401849988a7455f3fcc3baca
wenchangwu <wwc0524@163.com>
2017-09-24 23:49:24 +0800

    init

--- src/main/java/com/example/eureka/client/controller/HelloController.java
+++ src/main/java/com/example/eureka/client/controller/HelloController.java
@@ -1,8 +1,12 @@
 package com.example.eureka.client.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Description：
@@ -12,13 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
  * User: qingyunzi Date: 17-9-22 ProjectName:eureka-client Version:
  */
 @RestController
+@Slf4j
 public class HelloController {
 
+    @Autowired
     private DiscoveryClient client;
 
 
-    public String index(){
-        ServiceInstance instance=client.getLocalServiceInstance();
+    @RequestMapping("/index")
+    public String index() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        log.info("service provider:"+instance.getHost()+":"+instance.getPort());
         return "Hello World";
     }
 }
